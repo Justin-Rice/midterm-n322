96b364470f988fff2918f55a82ce7cb115dafaf8
jrice <58012439+Justin-Rice@users.noreply.github.com>
2021-10-13 15:51:40 -0400

    created homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.css';
-import { Header, Container, Button, Grid, List} from 'semantic-ui-react';
+import { Container, Segment, Header, Button} from 'semantic-ui-react';
  
 import NavBar from '../components/NavBar';
 
 
 const HomePage = () =>{
   return(
-   <NavBar/>
+
+    <React.Fragment>
+      <NavBar/>
+      {/* <Container>
+        what
+      </Container> */}
+
+      <Segment>
+        <Container textAlign='center'>
+          <Header>Home</Header>
+          <p>Some text that needs not readen</p>
+          <Button
+          color='green'
+          >
+            Join Now!
+          </Button>
+        </Container>
+
+      </Segment>
+      <Container>
+        <Header>News</Header>
+        <p>some more random text</p>
+      </Container>
+
+      
+    </React.Fragment>
+   
   )
 }
 
