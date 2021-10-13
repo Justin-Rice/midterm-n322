1809fb49648f9d6fcaa75231eafa8e4f99beb8f2
jrice <58012439+Justin-Rice@users.noreply.github.com>
2021-10-13 16:54:37 -0400

    readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Midterm
 
 Justin Rice 
 
+https://in-info-web4.informatics.iupui.edu/~jr16/n322/midterm-n322/
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~jr16/n322/midterm-n322",
     title: "midterm-n322",
   },
   plugins: [],
+  pathPrefix: '/~jr16/n322/midterm-n322'
 };
